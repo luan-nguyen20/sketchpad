b562f54c9e3da51231c2fbe900da22c8c2c6e4a1
Luan <nguyenbaoluan1989@gmail.com>
2020-01-19 21:27:17 -0600

    add createSquareGrid, createCell funcs, start listeners for cells

---
+++ script.js
@@ -0,0 +1,33 @@
+const gridContainer = document.querySelector('.gridContainer');
+
+window.onload = createSquareGrid(16);
+
+//create a grid with same number of rows and columns.
+//size = row num = column num
+function createSquareGrid(size){
+    //num of cells = size * size
+    for(i=0; i < size*size; i++){
+        createCell();
+    }
+    //num colums to create = size
+    //same as:
+    //.gridContainer {
+    //  grid-template-columns: repeat(size, 1fr);
+    //}
+    //fr unit is a fraction of the free space of the grid container.
+    gridContainer.style.gridTemplateColumns = 'repeat(' + size + ',1fr)';
+}
+
+function createCell(){
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    gridContainer.appendChild(cell);
+}
+
+const cells = document.querySelectorAll('.cell');
+//console.log(cells);
+cells.forEach(cell => cell.addEventListener('mouseover',changeBGColor));
+
+function changeBGColor(){
+    
+}
