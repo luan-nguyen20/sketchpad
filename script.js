73d5ee1d5eb3b0c31c615850e15ed4d5da340c65
Luan <nguyenbaoluan1989@gmail.com>
2020-01-19 21:54:36 -0600

    finish clear func and listener for clearBtn

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onload = createSquareGrid(3);
 function createSquareGrid(size){
     //num of cells = size * size
     for(i=0; i < size*size; i++){ createCell(); }
-    
+
     //num colums to create = size
     //same as:
     //.gridContainer {
@@ -17,13 +17,25 @@ function createSquareGrid(size){
     gridContainer.style.gridTemplateColumns = 'repeat(' + size + ',1fr)';
 }
 
+//create cell and add as child of container
 function createCell(){
     let cell = document.createElement('div');
     cell.classList.add('cell');
     gridContainer.appendChild(cell);
 }
 
+//mouse hover event listener for cells
 const cells = document.querySelectorAll('.cell');
 cells.forEach(cell => cell.addEventListener('mouseover',changeBGColor));
 
+//change color by adding class 'hover'
 function changeBGColor(e){ e.target.classList.add('hover'); }
+
+//event listener for Clear button
+const clearBtn = document.querySelector('.clearBtn');
+clearBtn.addEventListener('click',clear);
+
+//clear by removing class 'hover'
+function clear(){
+    cells.forEach(cell => cell.classList.remove('hover'));
+}
