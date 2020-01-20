32c150dde4b0408c0dc099782a45679089218fae
Luan <nguyenbaoluan1989@gmail.com>
2020-01-19 20:45:02 -0600

    started over

--- script.js
+++ script.js
@@ -2,16 +2,44 @@ const container = document.querySelector('.gridContainer');
 
 window.onload = createGrid;
 
-function createGrid(cellNum){
-    cellNum=256;
-    for(i=0; i < cellNum; i++){
-        createCell();
+let gridArray=[];
+
+function createGrid(cellCount){
+    cellCount=256;
+    for(i=0; i < cellCount; i++){
+        createCell(i);
     }
+    //gridArray = Array.from(document.querySelectorAll('.cell'));
+    //console.log(document.querySelectorAll('.cell'));
+    //console.log(gridArray);
 }
 
-function createCell(){
+//console.log(gridArray);
+
+function createCell(cellNum){
     let cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.textContent = 'hello'
+    cell.textContent = cellNum;
+
+    let aspectRatioBoxInside = document.createElement('div');
+    aspectRatioBoxInside.classList.add('aspectRatioBoxInside');
+    aspectRatioBoxInside.textContent = 'hi';
+    cell.appendChild(aspectRatioBoxInside);
+
     container.appendChild(cell);
+    gridArray.push(aspectRatioBoxInside);
+    //console.log(gridArray);
+}
+console.log(gridArray);
+
+//const cells = Array.from(document.querySelectorAll('.cell'));
+//console.log(cells);
+//console.log(document.querySelectorAll('.cell'));
+
+gridArray.forEach(cell => cell.addEventListener('click',changeBGColor));
+
+function changeBGColor(e){
+    //e.target.classList.add('hover');
+    //console.log(e.target);
+    console.log(e);
 }
